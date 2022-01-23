c32fa91e8acd7267a1d9b6e60a4e69338ea3847c
Omer Ali Malik <omeralimalik96@gmail.com>
2022-01-24 00:11:37 +0500

    Added react navigation and setup a basic navigator

--- App.js
+++ App.js
@@ -1,155 +1,25 @@
 import React from "react";
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import {SignupScreen} from './components/Signup';
 
 
-import {
-  Heading,
-  NativeBaseProvider,
-  Input,
-  Box,
-  Button,
-  Icon,
-  Text,
-  extendTheme,
-  VStack,
-} from "native-base";
-import { MaterialIcons  , Entypo , Ionicons} from "@expo/vector-icons"
+const Stack = createNativeStackNavigator();
 
-
-
-// Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-};
-
-// extend the theme
-export const theme = extendTheme({ config });
 
 export default function App() {
-
-  const [show, setShow] = React.useState(false)
-  const [email , setEmail] = React.useState(false);
-  const [name , setName] = React.useState(false);
-  const [userName , setUserName] = React.useState(false);
-  const [password , setPassword] = React.useState(false);
-  const [confirmedPassword , setConfirmedPassword] = React.useState(false);
-
-  const handleClick = () => setShow(!show)
-
   return (
-    <NativeBaseProvider>
-
-      <Box bg={"#AEFEFF"} padding={100}>
-      
-        <Heading textAlign={"center"} margin={17} color={"white"}>Register Account</Heading>
-        {/* <Center
-          _dark={{ bg: "blueGray.900" }}
-          _light={{ bg: "#EA99D5" }}
-          px={4}
-          flex={1}
-        > */}
-
-          {/* Vertical stack for the text inputs */}
-          <VStack space={5} alignItems="center">
-
-            {/* Name text Input */}
-            <Input 
-              placeholder="Name" bg={"white"} variant={"underlined"}
-              InputLeftElement={
-                <Icon
-                  as={<MaterialIcons name="person" />}
-                  size={3}
-                  ml="2"
-                  color="muted.400"
-                />
-              }
-              onChange={e => setName(e.target.value)}
-              ></Input>
-
-            {/* Email Text Input */}
-            <Input 
-              InputLeftElement={
-                <Icon
-                  as={<Entypo name="email" size={24} color="black" />}
-                  size={3}
-                  ml="2"
-                  color="muted.400"
-                />
-              }
-              placeholder="Email" bg={"white"} variant={"underlined"}
-              onChange={e => setEmail(e.target.value)}
-            ></Input>
-
-            {/* Username text Input */}
-            <Input 
-              placeholder="Username" bg={"white"} variant={"underlined"}
-              InputLeftElement={
-                <Icon
-                  as={<Entypo name="emoji-flirt" size={24} color="black" />}
-                  size={3}
-                  ml="2"
-                  color="muted.400"
-                />
-              }
-              onChange={e => setUserName(e.target.value)}
-            ></Input>
-
-            {/* Password Text Input */}
-            <Input
-              type={show ? "text" : "password"}
-              // InputRightElement={
-              //   <Button size="xs" rounded="none" w="1/6" h="full" onPress={handleClick}>
-              //     {show ? "Hide" : "Show"}
-              //   </Button>
-              // }
-              InputLeftElement={
-                <Icon
-                  as={<Ionicons name="key" size={24} color="black" />}
-                  size={3}
-                  ml="2"
-                  color="muted.400"
-                />
-              }
-              bg={"white"}
-              variant={"underlined"}
-              placeholder="Password"
-              onChange={e => setPassword(e.target.value)}
-            />
-
-            {/* Confirm Password Text Input */}
-            <Input
-                type={show ? "text" : "password"}
-                // InputRightElement={
-                //   <Button size="xs" rounded="none" w="1/6" h="full" onPress={handleClick}>
-                //     {show ? "Hide" : "Show"}
-                //   </Button>
-                // }
-                InputLeftElement={
-                  <Icon
-                    as={<Ionicons name="key" size={24} color="black" />}
-                    size={3}
-                    ml="2"
-                    color="muted.400"
-                  />
-                }
-                bg={"white"}
-                variant={"underlined"}
-                placeholder="Confirm password"
-                onChange={e => setConfirmedPassword(e.target.value)}
-              />
-
-              <Button onPress={() => {
-                alert(name);
-                alert(userName);
-                alert(email);
-                alert(password);
-                alert(confirmedPassword)
-              }}>Register!</Button>
-
-            </VStack>
-        </Box>
-      {/* </Center> */}
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={SignupScreen}></Stack.Screen>
+        <Stack.Screen name="Test" component={() => (
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Hello World!</Text>
+          </View>
+        )} options={{title : "Testing"}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ components/Signup.js
@@ -0,0 +1,153 @@
+import React from "react";
+
+import {
+    Heading,
+    NativeBaseProvider,
+    Input,
+    Box,
+    Button,
+    Icon,
+    extendTheme,
+    VStack,
+} from "native-base";
+import { MaterialIcons  , Entypo , Ionicons} from "@expo/vector-icons"
+  
+  
+  
+// Define the config
+const config = {
+useSystemColorMode: false,
+initialColorMode: "dark",
+};
+
+// extend the theme
+export const theme = extendTheme({ config });
+  
+  
+export function SignupScreen() {
+  
+    const [show, setShow] = React.useState(false)
+    const [email , setEmail] = React.useState(false);
+    const [name , setName] = React.useState(false);
+    const [userName , setUserName] = React.useState(false);
+    const [password , setPassword] = React.useState(false);
+    const [confirmedPassword , setConfirmedPassword] = React.useState(false);
+  
+    const handleClick = () => setShow(!show)
+  
+    return (
+      <NativeBaseProvider>
+  
+        <Box bg={"#AEFEFF"} padding={100}>
+        
+          <Heading textAlign={"center"} margin={17} color={"white"}>Register Account</Heading>
+          {/* <Center
+            _dark={{ bg: "blueGray.900" }}
+            _light={{ bg: "#EA99D5" }}
+            px={4}
+            flex={1}
+          > */}
+  
+            {/* Vertical stack for the text inputs */}
+            <VStack space={5} alignItems="center">
+  
+              {/* Name text Input */}
+              <Input 
+                placeholder="Name" bg={"white"} variant={"underlined"}
+                InputLeftElement={
+                  <Icon
+                    as={<MaterialIcons name="person" />}
+                    size={3}
+                    ml="2"
+                    color="muted.400"
+                  />
+                }
+                onChange={e => setName(e.target.value)}
+                ></Input>
+  
+              {/* Email Text Input */}
+              <Input 
+                InputLeftElement={
+                  <Icon
+                    as={<Entypo name="email" size={24} color="black" />}
+                    size={3}
+                    ml="2"
+                    color="muted.400"
+                  />
+                }
+                placeholder="Email" bg={"white"} variant={"underlined"}
+                onChange={e => setEmail(e.target.value)}
+              ></Input>
+  
+              {/* Username text Input */}
+              <Input 
+                placeholder="Username" bg={"white"} variant={"underlined"}
+                InputLeftElement={
+                  <Icon
+                    as={<Entypo name="emoji-flirt" size={24} color="black" />}
+                    size={3}
+                    ml="2"
+                    color="muted.400"
+                  />
+                }
+                onChange={e => setUserName(e.target.value)}
+              ></Input>
+  
+              {/* Password Text Input */}
+              <Input
+                type={show ? "text" : "password"}
+                // InputRightElement={
+                //   <Button size="xs" rounded="none" w="1/6" h="full" onPress={handleClick}>
+                //     {show ? "Hide" : "Show"}
+                //   </Button>
+                // }
+                InputLeftElement={
+                  <Icon
+                    as={<Ionicons name="key" size={24} color="black" />}
+                    size={3}
+                    ml="2"
+                    color="muted.400"
+                  />
+                }
+                bg={"white"}
+                variant={"underlined"}
+                placeholder="Password"
+                onChange={e => setPassword(e.target.value)}
+              />
+  
+              {/* Confirm Password Text Input */}
+              <Input
+                  type={show ? "text" : "password"}
+                  // InputRightElement={
+                  //   <Button size="xs" rounded="none" w="1/6" h="full" onPress={handleClick}>
+                  //     {show ? "Hide" : "Show"}
+                  //   </Button>
+                  // }
+                  InputLeftElement={
+                    <Icon
+                      as={<Ionicons name="key" size={24} color="black" />}
+                      size={3}
+                      ml="2"
+                      color="muted.400"
+                    />
+                  }
+                  bg={"white"}
+                  variant={"underlined"}
+                  placeholder="Confirm password"
+                  onChange={e => setConfirmedPassword(e.target.value)}
+                />
+  
+                <Button onPress={() => {
+                  alert(name);
+                  alert(userName);
+                  alert(email);
+                  alert(password);
+                  alert(confirmedPassword)
+                }}>Register!</Button>
+  
+              </VStack>
+          </Box>
+        {/* </Center> */}
+      </NativeBaseProvider>
+    );
+  }
